3db88b0f80f9220b003fc88eafb5dc9c0e89f979
Janeyziqinglin <janeyziqinglin@gmail.com>
2023-04-05 10:03:23 -0400

    implement logout user.

--- controllers/userController.js
+++ controllers/userController.js
@@ -110,4 +110,17 @@ const loginUser = asyncHandler(async (req, res) => {
     }
   });
 
-module.exports = {registerUser,loginUser};
+
+  //login user
+const logout = asyncHandler(async (req, res) => {
+    res.cookie("token", "",{
+        path:"/", 
+        httpOnly: true, 
+        expires:new Date(0), //expire the cookie
+        sameSite: "none",
+        secure: true,
+    });
+    return res.status(200).json({message: "successful logout"})
+});
+
+module.exports = {registerUser,loginUser,logout,};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {registerUser , loginUser} = require("../controllers/userController");
+const {registerUser , loginUser,logout} = require("../controllers/userController");
 
 
 //frontend send info and post to backend using the controller
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
+router.post("/logout", logout);
 
 module.exports = router;
