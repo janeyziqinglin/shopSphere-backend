face5dc2dff80677bf31d4c569a524dbda291fa5
Janeyziqinglin <janeyziqinglin@gmail.com>
2023-04-05 20:41:19 -0400

    implement reset password function and route.

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,6 +9,7 @@ const {
   updateUser,
   changePassword,
   forgotPassword,
+  resetPassword,
 } = require("../controllers/userController");
 const protect = require("../middleWare/authMiddleware");
 
@@ -21,5 +22,6 @@ router.get("/loggedin", loginStatus);
 router.patch("/updateuser", protect, updateUser);
 router.patch("/changepassword", protect, changePassword);
 router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resetToken", resetPassword);
 
 module.exports = router;
