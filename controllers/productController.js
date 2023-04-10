583190afd6e9dc8641b4b99301f6464c82efb71d
Janeyziqinglin <janeyziqinglin@gmail.com>
2023-04-10 11:37:18 -0400

    save product images to cloudinary.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const Product = require("../models/productModel");
 const { fileSizeFormatter } = require("../utils/fileUpload");
+const cloudinary = require("cloudinary").v2;
+
 const createProduct = asyncHandler(async (req, res) => {
   const { name, sku, category, quantity, price, description } = req.body;
   //validation
@@ -10,38 +12,38 @@ const createProduct = asyncHandler(async (req, res) => {
   }
 
   //handle image upload local
-  let fileData = {};
-  if (req.file) {
-    fileData = {
-      fileName: req.file.originalname,
-      filePath: req.file.path,
-      fileType: req.file.mimetype,
-      fileSize: fileSizeFormatter(req.file.size, 2),
-    };
-  }
-
-  // //handle image upload
-  // let fileData = {}
+  // let fileData = {};
   // if (req.file) {
-  //   // Save image to cloudinary
-  //   let uploadedFile;
-  //   try {
-  //     uploadedFile = await cloudinary.uploader.upload(req.file.path, {
-  //       folder: "Pinvent App",
-  //       resource_type: "image",
-  //     });
-  //   } catch (error) {
-  //     res.status(500);
-  //     throw new Error("Image could not be uploaded");
-  //   }
-
   //   fileData = {
   //     fileName: req.file.originalname,
-  //     filePath: uploadedFile.secure_url,
+  //     filePath: req.file.path,
   //     fileType: req.file.mimetype,
   //     fileSize: fileSizeFormatter(req.file.size, 2),
   //   };
   // }
+
+  //handle image upload
+  let fileData = {}
+  if (req.file) {
+    // Save image to cloudinary
+    let uploadedFile;
+    try {
+      uploadedFile = await cloudinary.uploader.upload(req.file.path, {
+        folder: "Pinvent App",
+        resource_type: "image",
+      });
+    } catch (error) {
+      res.status(500);
+      throw new Error("Image could not be uploaded");
+    }
+
+    fileData = {
+      fileName: req.file.originalname,
+      filePath: uploadedFile.secure_url,
+      fileType: req.file.mimetype,
+      fileSize: fileSizeFormatter(req.file.size, 2),
+    };
+  }
 
   //create product (skip image)
   const product = await Product.create({
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,7 +3,6 @@ const { createProduct } = require("../controllers/productController");
 const router = express.Router();
 const protect = require("../middleWare/authMiddleware");
 const { upload } = require("../utils/fileUpload");
-
 router.post("/", protect,upload.single("image"), createProduct);
 
 
