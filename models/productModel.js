fa53516e3c3cbbdf60983346ac8cd2ad5f3b4653
JaneyLin <105125897+janeyziqinglin@users.noreply.github.com>
2023-04-16 23:53:29 -0400

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -36,7 +36,7 @@ const productSchema = mongoose.Schema(
     },
     description: {
       type: String,
-      required: [true, "Please add a description"],
+      required: [true, "Please add a description of the product."],
       trim: true,
     },
     image: {
